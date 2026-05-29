54ce6495936c89db9aab07c5a2e536b650a52e34
Grzegorz Kowal <grzegorz@amuncode.org>
2026-05-29 20:37:41 -0300

    Do not pull all parameters if CGL=False
    Signed-off-by: Grzegorz Kowal <grzegorz@amuncode.org>

--- src/tearing_eigenmodes/physics.py
+++ src/tearing_eigenmodes/physics.py
@@ -63,25 +63,25 @@ def estimate_max(params: SimulationParams) -> float:
     CGL          = params.CGL
     S            = params.S
     Pr           = params.Pr
-    ɣpar         = params.parallel_index
-    ɣper         = params.perpendicular_index
 
     if S is None or S <= 0:
         raise ValueError("Lundquist number S must be positive.")
     if Pr is None or Pr < 0:
         raise ValueError("Prandtl number Pr cannot be negative.")
-    if ɣpar <= 0:
-        raise ValueError("Parallel adiabatic index ɣpar must be positive.")
-    if ɣper <= 0:
-        raise ValueError("Perpendicular adiabatic index ɣper must be positive.")
 
     αm = 1.3583e+00 * (S / (S + 400))**0.25 * S**-0.25 * ((0.05*Pr**2+0.7*Pr+1)/(12*Pr+1))**0.125
 
     if CGL:
         β            = params.plasma_beta
         Δβ           = params.plasma_beta_difference
+        ɣpar         = params.parallel_index
+        ɣper         = params.perpendicular_index
         if β < 0:
             raise ValueError("Plasma beta must be positive.")
+        if ɣpar <= 0:
+            raise ValueError("Parallel adiabatic index ɣpar must be positive.")
+        if ɣper <= 0:
+            raise ValueError("Perpendicular adiabatic index ɣper must be positive.")
 
         A, R0 = calculate_cgl_factors(
             beta=β,
